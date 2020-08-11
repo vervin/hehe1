b40a56d56c42d65717b7c9d54be3d264ca52a05a
vervin <vervin@163.com>
2020-08-11 10:25:25 +0800

    add 1

---
+++ hehe.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2020-08-11 10:24:14
+ * @LastEditTime: 2020-08-11 10:24:27
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \learngit\hehe.js
+ */
+console.log(1)
