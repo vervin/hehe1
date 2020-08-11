ed0b8b46de4139e87bd51c241daa4e325de3cf76
vervin <vervin@163.com>
2020-08-11 10:28:51 +0800

    add5

---
+++ hehe.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2020-08-11 10:28:15
+ * @LastEditTime: 2020-08-11 10:28:26
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \haha1\hehe1\hehe.js
+ */
+console.log(5)
